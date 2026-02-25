4d433aff13589ad4337e10efec91067612fa93b6
FelixFelicis99 <79476414+FelixFelicis99@users.noreply.github.com>
2026-02-25 19:33:35 +0000

    change bg colour

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,23 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [counter,setCounter] = useState(15);
-  //let counter = 15;w
-  const addvalue=()=>{
-    counter=counter+1;
+  let [counter,setCounter]= useState("black");
+  // document.getElementById('root').style.backgroundColor = counter
+
+  const changebg = (event)=>{
+    const colour = event.currentTarget.dataset.color
+    console.log(colour, Math.random());
+    counter=colour;
     setCounter(counter);
+    // document.getElementById('root').style.backgroundColor = counter
+    // counter=colour;
+    // setCounter(counter);
+    // console.log("red");
   }
-  const removevalue=()=>{
-    counter=counter-1;
-    setCounter(counter);
-  }
+  
   return (
-    <>
-      <h1>React app 2</h1>
-      <h2>current count is {counter}</h2>
-      <button onClick={addvalue}>add value</button>
-      <button onClick={removevalue}>decrease value</button>
-    </>
+    
+    <div className="w-full h-screen duration-200" style={{backgroundColor: counter}}>
+    <div className="fixed bottom-6 left-0 right-0 flex justify-center gap-4">
+      {/* document.getElementById('root').style.backgroundColor=counter; */}
+      
+      <button data-color="red" onClick={changebg} className="bg-red-500 text-white">red</button> 
+      <button data-color="blue" onClick={changebg} className="bg-blue-500 text-white rounded">blue</button>
+      <button data-color="green" onClick={changebg} className="bg-green-500 text-white rounded">green</button>
+      <button data-color="yellow" onClick={changebg} className="bg-yellow-500 text-white rounded">yellow</button>
+      <button data-color="orange" onClick={changebg} className="bg-orange-500 text-white rounded">orange</button>
+      <button data-color="purple" onClick={changebg} className="bg-purple-500 text-white rounded">lavender</button>
+    </div>
+    </div>
+    
   )
 }
 
